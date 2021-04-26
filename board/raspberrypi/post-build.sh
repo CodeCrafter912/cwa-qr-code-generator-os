53ecc5d5ba0b3445cf0df69df263412a4126e020
Dorian Zedler <dorian@itsblue.de>
2021-04-26 12:56:00 +0200

    Initially ceck in working config

---
+++ board/raspberrypi/post-build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -u
+set -e
+
+# create mountpoint for boot fs
+mkdir -p $TARGET_DIR/boot
+
+# patch config.txt file
+CONFIG_TXT_FILE_PATH="$BINARIES_DIR/rpi-firmware/config.txt"
+
+#APPEND_LINES="boot_delay=1 disable_splash=1 dtparam=audio=on"
+APPEND_LINES="boot_delay=1"
+
+for LINE in $APPEND_LINES; do
+    if [ -z $(grep "$LINE" "$CONFIG_TXT_FILE_PATH")]; then
+        echo $LINE >> $CONFIG_TXT_FILE_PATH
+    fi
+done
+
+# patch cmdline.txt file
+CMDLINE_TXT_FILE_PATH="$BINARIES_DIR/rpi-firmware/cmdline.txt"
+
+#APPEND_PARAMETERS="logo.nologo quiet vt.global_cursor_default=0"
+APPEND_PARAMETERS=""
+for PARAMETER in $APPEND_PARAMETERS; do
+    if [ -z $(grep "$PARAMETER" "$CMDLINE_TXT_FILE_PATH")]; then
+        sed -i " 1 s/.*/& $PARAMETER/" $CMDLINE_TXT_FILE_PATH
+    fi
+done
+
+#sed -i 's/console=tty1/console=tty3/g' $CMDLINE_TXT_FILE_PATH
+
+# Add a console on tty1
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+	sed -i '/GENERIC_SERIAL/a\
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+fi
+
+
+exit 0
